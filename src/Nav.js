e4765db7cfee9daae2e70ec42ae02421735e7ba7
Dhananjai sharma <dhananjaisharma09@gmail.com>
2022-01-11 00:35:34 +0530

    fixes

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useEffect , useState} from 'react'
 import './Nav.css'
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () =>{
+            if (window.scrollY > 100){
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav-kuro"}`}>
             <img 
                 className="nav-logo"
                 src="https://lh3.googleusercontent.com/yRzaF4RP7GL558uptij8ySaCBmxHCa4m1M5ian6qC5cwqTSIECuqOVLVLRatXkgfSQFqaPX4V2inoZouoCEOPScLrT4FYzIpEgLwqug_EVn8QvWJF1Www9wrxyn4-PjHVRr2-XO9fA=w474-h148-p-k" 
