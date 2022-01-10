cb4cd615136da4e2901e849ec7956c41e8080bbd
Dhananjai sharma <dhananjaisharma09@gmail.com>
2022-01-10 23:04:03 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Row.js'
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>netflix</h1>
+        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row title="Trending now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "ec6976196e6ad5a9036a269ee9b37a45";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
