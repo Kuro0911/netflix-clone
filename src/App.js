f2dd14808c5a2cfde57312004dec57d97ebe41cd
Dhananjai sharma <dhananjaisharma09@gmail.com>
2022-01-10 23:19:24 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <h1>netflix</h1>
-        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
         <Row title="Trending now" fetchUrl={requests.fetchTrending} />
         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from './axios'
 import'./Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     
     const [movies, setMovies] = useState([]);
     
@@ -21,7 +21,7 @@ function Row({ title, fetchUrl }) {
             <h2>{title}</h2>
             <div className="row-posters">
                     {movies.map(movie => (
-                        <img key={movie.id} className="row-poster" src={`${base_url}${movie.poster_path} `} alt={movie.name}/>
+                        <img key={movie.id} className={`row-poster ${isLargeRow && "row-poster-large"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path} `} alt={movie.name}/>
                     ))}
             </div>
         </div>
